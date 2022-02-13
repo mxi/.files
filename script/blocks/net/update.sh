72660c57f1c595f36d281e2645c37a7f62d4b225
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2022-02-13 09:44:33 -0800

    dwmblocks: Add VPN string to dwm network block

--- script/blocks/net/update.sh
+++ script/blocks/net/update.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
 COLOR=$DWM_CLR_WHT
-NAME=$(nmcli c s --active | awk '
-	$1 != "NAME" {
-		print $1;
+NETINFO=$(nmcli -t c s --active | awk -v FS=':' '
+	BEGIN {
+		VPN  = "";
+		TUN  = "";
+		CONN = "";
+	}
+
+	$3 == "vpn" {
+		VPN = $1;
+	}
+	
+	$3 == "tun" {
+		TUN = $1;
+	}
+
+	$3 != "vpn" && $3 != "tun" {
+		CONN = $1;
+	}
+
+	END {
+		print VPN ":" TUN ":" CONN;
 	}
 ')
-[ -z "$NAME" ] && NAME="No Connection" && COLOR=$DWM_CLR_RED
 
+VPN=$(echo "$NETINFO" | cut -d: -f1)
+TUN=$(echo "$NETINFO" | cut -d: -f2)
+CONN=$(echo "$NETINFO" | cut -d: -f3)
 
-$PRINTF "$COLOR %s$DWM_CLR_STD" "$NAME"
-
+if [ -z "$CONN" ]; then
+	$PRINTF "$DWM_CLR_RED %s$DWM_CLR_STD" "ECONN"
+elif [ -z "$VPN" ]; then
+	$PRINTF "$DWM_CLR_WHT %s$DWM_CLR_STD" "$CONN"
+else
+	$PRINTF "$DWM_CLR_WHT %s$DWM_CLR_STD/$DWM_CLR_BLU%s$DWM_CLR_STD" "$CONN" "$VPN"
+fi
